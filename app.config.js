8511d2c5b7e43b1106f5f662b11bc49029f8986c
Luiz Henrique Proença <luiz.proenca@yahoo.com.br>
2026-04-14 23:11:24 -0300

    Primeira versão para ios publicada no testfligh

--- app.config.js
+++ app.config.js
@@ -5,26 +5,62 @@ module.exports = {
     name: 'SpinShot',
     slug: 'spinshot',
     version: versionData.version,
-
+    icon: './assets/images/icon.png',
+    orientation: 'portrait',
+    scheme: 'onspaceapp',
+    userInterfaceStyle: 'dark',
+    newArchEnabled: true,
     ios: {
-      buildNumber: String(versionData.build), // iOS precisa ser string
-      bundleIdentifier: 'com.ironman.spinshot.app',
+      appleTeamId: 'Q6943C4X4V',
+      usesNonExemptEncryption: false,
       supportsTablet: true,
+      bundleIdentifier: 'com.ironman.spinshot.app',
+      buildNumber: String(versionData.build),
       infoPlist: {
         UIStatusBarStyle: 'UIStatusBarStyleLightContent',
       },
     },
-
     android: {
-      versionCode: versionData.build, // Android precisa ser number
       package: 'com.ironman.spinshot.app',
+      versionCode: versionData.build,
       adaptiveIcon: {
         foregroundImage: './assets/images/logo.png',
         backgroundColor: '#000000',
       },
       edgeToEdgeEnabled: true,
     },
-
-    // resto igual...
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/logo.png',
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-splash-screen',
+        {
+          image: './assets/images/logo.png',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#1A1740',
+        },
+      ],
+      'expo-web-browser',
+      '@react-native-community/datetimepicker',
+      'expo-asset',
+      'expo-audio',
+      'expo-font',
+      'expo-localization',
+      'expo-mail-composer',
+      'expo-secure-store',
+      'expo-sqlite',
+      'expo-video',
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      cloudinaryCloudName: process.env.EXPO_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    },
   },
 };
